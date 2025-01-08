700926e1a3e6f8cc9f82b689503a82f43fee1c16
jtoledom1 <jtoledom1@outlook.com>
2025-01-07 23:50:30 -0600

    e

--- HTML/Forms_Validation_JS_v2/app.js
+++ HTML/Forms_Validation_JS_v2/app.js
@@ -1,32 +1,34 @@
-const email = document.getElementById("mail");
-
-email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
-  } else {
-    email.setCustomValidity("");
-  }
-});
-
-
-function color(label) {
-    label.style.color = "red";
-    setTimeout(() => {
-        label.style.color = "rgba(255,0,0,0.9)";
-    }, 300);
+function display(){
+    clear();
+    const elemshow = document.getElementById('li_req');
+    document.getElementById('frm_req').style.display='block';
+    elemshow.style.color='red';
 }
 
-function calls(tab, texth1, html) {
-    const intertab = document.getElementById(tab);
-    const jscontent = document.getElementById("jscontent");
-    const title = document.getElementById("title");
-    const varhtml = document.getElementById(html).innerHTML;
-    
-    title.innerText = texth1;
-    color(intertab);
-    jscontent.innerHTML = varhtml;
+function clear(){
+    const elementos = document.querySelectorAll('.nodisplay');
+    const ids = [];
+
+    elementos.forEach(elemento => {
+        ids.push(elemento.id);
+    });
+
+    elementos.forEach(elemento => {
+        elemento.style.display = 'none';
+        elemento.style.color = 'black';
+    });
+
 }
 
-function initial() {
-    document.getElementById('sec_Hom').hidden = false;
+function email(){
+    display();
+    const email = document.getElementById("mail");
+
+    email.addEventListener("input", (event) => {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I am expecting an email address!");
+    } else {
+        email.setCustomValidity("");
+    }
+    });
 }
