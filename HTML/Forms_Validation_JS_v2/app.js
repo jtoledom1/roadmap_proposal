7871980542a666dab1c1b133858d1ae85c5798ee
jtoledom1 <jtoledom1@outlook.com>
2025-01-13 20:16:24 -0600

    Validation main background color

--- HTML/Forms_Validation_JS_v2/app.js
+++ HTML/Forms_Validation_JS_v2/app.js
@@ -6,13 +6,32 @@ function show(targ){
 function hidehome(){
     let hom = document.getElementById('sechome');
     let form = document.getElementById('reqform');
+    let formMIL = document.getElementById('forMil');
+    let formMAL = document.getElementById('forMAl');
     hom.style.display='none';
     form.style.display='none';
+    formMIL.style.display='none';
+    formMAL.style.display='none';
+
 }
 function validateForm() {
     let x = document.forms["reqform"]["reqinput"].value;
     if (x == "") {
         alert("Name must be filled out");
+        return false;
+    }
+}
+function validateFormMIL(){
+    let x = document.forms["forMil"]["minLinput"].value.length;
+    if (x <= 3) {
+        alert("Form must contain at least 3 characters");
+        return false;
+    }
+}
+function validateFormMAL(){
+    let x = document.forms["forMAl"]["mAXLinput"].value.length;
+    if (x >= 3) {
+        alert("Form must contain less than 3 characters");
         return false;
     }
 }
